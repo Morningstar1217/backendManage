0cc103671676b48d76af91b8647c61ea2ad5162a
zhuzhaoqing <2702155238@qq.com>
2021-09-02 20:56:27 +0800

    增加功能

--- components/pageInfo.js
+++ components/pageInfo.js
@@ -5,8 +5,17 @@ Vue.component('page-info', {
             type: Boolean,
         },
     },
+    data() {
+        return {
+            isOpen: false,
+        }
+    },
     template: `
-      <div class="page-info" :class="{closeMenu:isCollapse}">
+      <div class="page-info" :class="{closeMenu:isCollapse,changeHeight:isOpen}">
+      <div class="open-or-close">
+        <i class="el-icon-arrow-up pointer" v-if="!isOpen" @click="isOpen = true"></i>
+        <i class="el-icon-arrow-down pointer" v-if="isOpen" @click="isOpen = false"></i>
+      </div>
       <div class="font-weight">使用说明：</div>
       <div class="info-content">
         本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说明，本页设置说
